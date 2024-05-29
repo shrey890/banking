95190ffa76539564a965c3637306048d5f836911
shrey890 <shrey90@proton.me>
2024-05-29 11:21:01 +0530

    route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,30 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-	return <div>Home</div>;
+	const loggedIn = { firstName: "Shrey" };
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="welcome"
+						user={loggedIn?.firstName || "Guest"}
+						subtext="Access and manage your account and transactions efficiently"
+					/>
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.92}
+					/>
+				</header>
+				Recent transactions
+			</div>
+			<RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+		</section>
+	);
 };
 
 export default Home;
